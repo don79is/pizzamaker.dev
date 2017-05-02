8bbe37621ca8328107398e6b6ed9984a0a7a4163
don79is <don79is@gmail.com>
2017-05-02 15:19:03 +0300

    models fix

--- app/Models/DTPizzaCheese.php
+++ app/Models/DTPizzaCheese.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class DTPizzaCheese extends DTCoreModel
 {
     protected $table = 'DT_pizza_cheese';
-    protected $fillable = ['name','calories'];
+    protected $fillable = ['id','name','calories'];
 }
